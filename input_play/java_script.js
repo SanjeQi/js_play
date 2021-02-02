39150eee62ce9918a3860e299a5c41aca2bf3928
SanjeQi <cristi.nord@gmail.com>
2021-02-02 18:45:26 +0000

    how many fingers

--- input_play/java_script.js
+++ input_play/java_script.js
@@ -1,6 +1,13 @@
-let newText = document.getElementById('myInput').value;
+document.getElementById('btn').onclick = function () {
+  // Generate a random number from 1 to 10
+  let x = Math.random();
+  x = x * 11;
+  x = Math.floor(x);
 
-document.getElementById('styleChanger').onclick = function () {
-  newText = document.getElementById('myInput').value;
-  document.getElementById('firstDiv').innerHTML = newText;
+  //  Check if the number put in by user is equal to the random number generated
+  if (document.getElementById('answer').value == x) {
+    alert(`Correct Guess. I was holding up ${x}`);
+  } else {
+    alert(`Wrong!!!!  Guess Again. I was holding up ${x} `);
+  }
 };
