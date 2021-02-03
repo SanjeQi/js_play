446cb0511efd789f8486625c46501c64928aba66
SanjeQi <cristi.nord@gmail.com>
2021-02-03 20:56:39 +0000

    guess my number

--- input_play/java_script.js
+++ input_play/java_script.js
@@ -1,13 +1,18 @@
-document.getElementById('btn').onclick = function () {
-  // Generate a random number from 1 to 10
-  let x = Math.random();
-  x = x * 11;
-  x = Math.floor(x);
+document.getElementById('guess').onclick = function () {
+  let gotit = false;
+  let guesses = 0;
+  let randomNumber;
 
-  //  Check if the number put in by user is equal to the random number generated
-  if (document.getElementById('answer').value == x) {
-    alert(`Correct Guess. I was holding up ${x}`);
-  } else {
-    alert(`Wrong!!!!  Guess Again. I was holding up ${x} `);
+  while (gotit == false) {
+    randomNumber = Math.floor(Math.random() * 11);
+
+    if (document.getElementById('myNumber').value == randomNumber) {
+      gotit = true;
+    } else {
+      guesses++;
+    }
   }
+  document.getElementById(
+    'answer'
+  ).innerHTML = `Cool!! I got it! It was  number ${randomNumber}.  It took me ${guesses} tries to guess your number. Not bad ha!?`;
 };
