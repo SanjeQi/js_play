5ce06b900261e67f555cd31117ba092695a9ded3
SanjeQi <cristi.nord@gmail.com>
2021-02-04 18:10:48 +0000

    Reaction tester game 2

--- reaction_tester/java_script.js
+++ reaction_tester/java_script.js
@@ -1,18 +1,22 @@
-makeBox = function () {
-  const randomTimer = function () {
-    //random number between 1 and 5001
-    let random = Math.floor(Math.random() * 5001);
-    console.log(random);
-    return random;
-  };
+let clickedTime, createdTime, reactionTime;
+
+function makeBox() {
+  //random number between 1 and 5001
+  let time = Math.random() * 5001;
 
   setTimeout(function () {
     document.getElementById('box_1').style.display = 'block';
-  }, randomTimer());
-};
+    createdTime = Date.now();
+    console.log('createdTime', createdTime);
+  }, time);
+}
 
 document.getElementById('box_1').onclick = function () {
+  clickedTime = Date.now();
   this.style.display = 'none';
+  reactionTime = (clickedTime - createdTime) / 1000;
+  console.log(reactionTime);
+  document.getElementById('time').innerHTML = `${reactionTime}s`;
   makeBox();
 };
 
