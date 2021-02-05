e95e41570322ec375c309eac9bcbe33f7063d248
SanjeQi <cristi.nord@gmail.com>
2021-02-05 20:27:56 +0000

    Clean

--- reaction_tester/java_script.js
+++ reaction_tester/java_script.js
@@ -13,7 +13,7 @@ function getRandomColor() {
 function getRandomShape() {
   let randomNumber = Math.floor(Math.random() * 51);
   let percentage = randomNumber.toString() + '%';
-  console.log(percentage);
+
   return percentage;
 }
 getRandomShape();
@@ -23,26 +23,25 @@ function makeBox() {
   let time = Math.random() * 5001;
 
   setTimeout(function () {
-    let top = Math.random() * 300;
+    let top = Math.random() * 500;
     let left = Math.random() * 600;
-    console.log(top, left);
-    document.getElementById('box_1').style.top = top + 'px';
-    document.getElementById('box_1').style.left = left + 'px';
 
-    document.getElementById('box_1').style.display = 'block';
-    document.getElementById('box_1').style.backgroundColor = getRandomColor();
-    document.getElementById('box_1').style.borderRadius = getRandomShape();
+    document.getElementById('box').style.top = top + 'px';
+    document.getElementById('box').style.left = left + 'px';
+
+    document.getElementById('box').style.display = 'block';
+    document.getElementById('box').style.backgroundColor = getRandomColor();
+    document.getElementById('box').style.borderRadius = getRandomShape();
 
     createdTime = Date.now();
-    console.log('createdTime', createdTime);
   }, time);
 }
 
-document.getElementById('box_1').onclick = function () {
+document.getElementById('box').onclick = function () {
   clickedTime = Date.now();
   this.style.display = 'none';
   reactionTime = (clickedTime - createdTime) / 1000;
-  console.log(reactionTime);
+
   document.getElementById('time').innerHTML = `${reactionTime}s`;
   makeBox();
 };
